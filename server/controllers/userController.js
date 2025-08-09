7015969077eedc70184aa98012d3efa1bd9b65af
Hemaprakash123 <hemaprakashreddy73@gmail.com>
2025-08-09 18:54:39 +0530

    backend complete

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,6 +4,8 @@ import imagekit from "../configs/imageKit.js"
 import Connection from "../models/Connection.js"
 import User from "../models/User.js"
 import fs from 'fs'
+import Post from "../models/Post.js"
+import { inngest } from "../ingest/index.js"
 
 export const getUserData=async(req,res)=>{
     try {
@@ -197,10 +199,16 @@ export const sendConnectionRequest=async(req,res)=>{
         })
 
         if(!connection){
-            await Connection.create({
+            const newConnection=await Connection.create({
                 from_user_id:userId,
                 to_user_id:id
             })
+
+            await inngest.send({
+                name:'app/connection-request',
+                data:{connectionId:newConnection._id}
+            })
+            
             return  res.json({success:true,message:'Connection request sent successfully'})
         }
         else if(connection && connection.status==='accepted'){
@@ -262,5 +270,22 @@ export const acceptConnectionRequest=async(req,res)=>{
     } catch (error) {
         console.log(error);
         res.json({success:false,message:error.message})
+    }
+}
+
+// get user profile
+export const getUserProfiles=async(req,res)=>{
+    try {
+        const {profileId}=req.body
+        const profile = await User.findById(profileId)
+        if(!profile){
+            return res.json({success:false,message:"profile not found"})
+        }
+        const post=await Post.find({user:profileId}).populate('user')
+
+        res.json({success:true,profile,posts})
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
     }
 }
--- server/ingest/index.js
+++ server/ingest/index.js
@@ -2,6 +2,8 @@ import { Inngest } from "inngest";
 import User from "../models/User.js";
 import Connection from "../models/Connection.js";
 import sendEmail from "../configs/nodeMailer.js";
+import Story from '../models/Story.js'
+import Message from "../models/Message.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "helta-app" });
@@ -12,7 +14,7 @@ const syncUserCreation = inngest.createFunction(
   async ({ event }) => {
     try {
       const { id, first_name, last_name, email_addresses, image_url } = event.data;
-      
+
       // Validate required fields
       if (!email_addresses?.[0]?.email_address) {
         throw new Error('No email address provided');
@@ -72,7 +74,7 @@ const syncUserDeletion = inngest.createFunction(
   async ({ event }) => {
     try {
       const { id } = event.data;
-      
+
       if (!id) {
         throw new Error('No user ID provided');
       }
@@ -90,7 +92,7 @@ const sendNewConnectionRequestReminder = inngest.createFunction(
   { event: "app/connection-request" },
   async ({ event, step }) => {
     const { connectionId } = event.data;
-    
+
     await step.run('send-connection-request-mail', async () => {
       const connection = await Connection.findById(connectionId).populate('from_user_id to_user_id');
       const subject = `👍 New Connection Request`;
@@ -112,7 +114,7 @@ const sendNewConnectionRequestReminder = inngest.createFunction(
 
     const in24Hours = new Date(Date.now() + 24 * 60 * 60 * 1000);
     await step.sleepUntil("wait-for-24hours", in24Hours);
-    
+
     await step.run('send-connection-request-reminder', async () => {
       const connection = await Connection.findById(connectionId).populate('from_user_id to_user_id');
 
@@ -141,9 +143,65 @@ const sendNewConnectionRequestReminder = inngest.createFunction(
   }
 );
 
+//to delete story after 24 hrs
+
+const deleteStory = inngest.createFunction(
+  { id: 'story-delete' },
+  { event: 'app/story.delete' },
+  async ({ event, step }) => {
+    const { storyId } = event.data;
+    const in24Hours = new Date(Date.now() + 24 * 60 * 60 * 1000)
+    await step.sleepUntil('wait-for-24-hours', in24Hours)
+    await step.run("delete-story", async () => {
+      await Story.findByIdAndDelete(storyId)
+      return { message: "story deleted" }
+    })
+  }
+)
+
+const sendNotificationOfUnseenMessages = inngest.createFunction(
+  { id: "send-unseen-messages-notification" },
+  { cron: "TZ=America/New_York 0 9 * * *" },//every day 9 am
+  async ({ step }) => {
+    const messages = await Message.find({ seen: false }).populate('to_user_id');
+    const unseenCount = {}
+
+    messages.map(message => {
+      unseenCount[message.to_user_id._id] = (unseenCount[message.to_user_id._id] || 0) + 1;
+    })
+
+    for (const userId in unseenCount) {
+      const user = await User.findById(userId)
+      const subject = `you have ${unseenCount[userId]} unseen messages`;
+      const body = `
+      <div style='font-family:Arial,sans-serif;padding:20px'>
+          <h2>Hi ${user.full_name},</h2>
+          <p>You have ${unseenCount[userId]} unseen messages</p>
+          <p>Click <a href="${process.env.FRONTEND_URL}/messages" style="color: #10b981">here</a>to view them</p>
+          <br/>
+          <p>Thanks,<br/>Helta-stay connected</p>
+      </div>      
+      `;
+
+      await sendEmail({
+        to:user.email,
+        subject,
+        body
+      })
+    }
+
+    return {message:'Notification sent.'}
+
+  })
+
+
+
 export const functions = [
   syncUserCreation,
   syncUserUpdation,
   syncUserDeletion,
-  sendNewConnectionRequestReminder
+  sendNewConnectionRequestReminder,
+  deleteStory,
+  sendNotificationOfUnseenMessages
 ];
+
